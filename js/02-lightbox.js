f6f80a65eea3c40fceaf11b045118ee2166a1b46
Danylo Bay <danylobay@gmail.com>
2023-02-08 16:04:30 +0100

    02-lightbox
    Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMakup(galleryItems);
+
+function createGalleryMakup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onContainerGalleryClick);
+
+function onContainerGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  new SimpleLightbox(".gallery .gallery__item", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
